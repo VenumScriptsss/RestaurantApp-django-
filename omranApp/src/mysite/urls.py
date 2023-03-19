2c5a7f3841c58b330f0d6a7a7abe663b549783c9
yahia <you@example.com>
2023-03-19 12:59:38 +0100

    page editerAjouterComm done

--- omranApp/src/mysite/urls.py
+++ omranApp/src/mysite/urls.py
@@ -31,5 +31,7 @@ urlpatterns = [
     path('ajouterEditer-product', ajouter_modifier_product, name='ajouterEditer-product'),
     path('editer-products', editProdsView, name='editer-products'),
     path('delete_product', delete_product, name='delete_product'),
+    path('home', homeView, name='home'),
+
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
